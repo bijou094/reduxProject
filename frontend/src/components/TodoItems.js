401d65703d15d0db21c8afab70911bdcba5415aa
bouaka houda <bouaka.houda@outlook.fr>
2022-02-01 14:28:33 +0100

    connecter le store

--- frontend/src/components/TodoItems.js
+++ frontend/src/components/TodoItems.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const TodoItems = (props) => {
+const TodoItems = ({todo}) => {
+    
     return (
+
         <>
             <li className="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action">
-                <span>X</span>
+                <span>{todo.name}</span>
                 <span>
                     <input className='mx-3' type='checkbox' />
                     <button className="btn btn-sm btn-danger"> Delete</button>
--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,15 +1,43 @@
+import { connect } from 'react-redux';
 import React from 'react';
 import TodoItems from './TodoItems';
+import { selectChoice} from '../store/actions'
 
-const TodoList = (props) => {
+const TodoList = ({todos}) => {
     return (
         <div>
             <ul className='list-group'>
-                <TodoItems />
+                {todos && todos.map(t =>(
+                    <TodoItems key={t.name} todo={t}/>
+                ))}
+                
             </ul>
 
         </div>
     );
 }
 
-export default TodoList;
+export default connect((state) =>{
+    const filter =state.filter;
+    let todos;
+    switch(filter){
+        case selectChoice.SHOW_DONE:{
+            todos=state.todos.filter(t => t.done)
+            break
+        }
+        case selectChoice.SHOW_ACTIVE: {
+            todos = state.todos.filter(t => t.done)
+            break
+        }
+        default :{
+            todos=state.todos
+            break
+        }
+    }
+    // il va retourné un objet les props du composant
+    return{
+        //la clés si nom de proprité et la valeur la partie de state qu'on veut
+        todos 
+    }
+}
+)(TodoList)
